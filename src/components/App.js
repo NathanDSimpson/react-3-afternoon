9f17fc700fa589301c69de727743338844f86f23
Nathan Simpson <ndsimpson01@gmail.com>
2019-04-04 16:42:12 -0600

    Completed. Did not do black-diamond task

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
@@ -19,19 +20,39 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then( res => {
+      console.log(res)
+      let posts = res.data
+      this.setState({
+        posts: posts
+      })
+    })
+    .catch(err => {
+      console.log("there was an error in component did mount", err)
+    })
   }
 
-  updatePost() {
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then(res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   
   }
 
-  deletePost() {
-
+  deletePost( id ) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`).then( results => {
+      this.setState({ posts: results.data });
+    });
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text }).then( results => {
+      this.setState({ posts: results.data });
+    });  
   }
 
   render() {
@@ -43,8 +64,17 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost}
+/>
+          {posts.map(post => (
+          <Post 
+          text={post.text} 
+          id={post.id} 
+          date={post.date} 
+          key={post.id}
+          updatePostFn={this.updatePost}
+          deletePostFn={this.deletePost}
+          />))}
         </section>
       </div>
     );
